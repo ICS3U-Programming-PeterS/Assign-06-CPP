6245463fee10ec9a9bda1def1869b9163d008198
Peter Sobowale <peter.sobowale@stu.ocsb.ca>
2022-12-29 18:56:15 +0000

    Added fileInput.cpp

--- fileInput.cpp
+++ fileInput.cpp
@@ -17,22 +17,21 @@
 
 // function to split the list in the middle
 void SplitList(std::list<std::string> lst,
- std::list<std::string> firstHalf, std::list<std::string> secondHalf) {
+std::list<std::string> firstHalf, std::list<std::string> secondHalf) {
     // calculate the middle index of the list
-    float middle = lst.size() / 2.0;  // use floating point division to get
-                                      // a properly rounded middle index
+    float middle = lst.size() / 2.0;  // keep as a float
     middle = ceil(middle);            // round up to the nearest integer
 
-    // iterate over the first half of the list and append the elements to
-    // the firstHalf list
+    // iterate over the first half of the list
+    // aappend the elements to the firstHalf list
     for (int i = 0; i < middle; i++) {
         auto it = lst.begin();
         std::advance(it, i);
         firstHalf.push_back(*it);
     }
 
-    // iterate over the second half of the list and append the elements
-    // to the secondHalf list
+    // iterate over the second half of the list
+    // append the elements to the secondHalf list
     for (int i = middle; i < lst.size(); i++) {
         auto it = lst.begin();
         std::advance(it, i);
@@ -43,9 +42,9 @@ void SplitList(std::list<std::string> lst,
 int main() {
     // prompt the user
     std::cout << "This program reads a list of elements from a "
-    << "text file or gets the input from user. Then splits it in "
-    << "the middle and stores the elements in two different "
-    << "lists using pass by reference\n"
+              << "text file or gets the input from user. Then splits"
+              << " it in the middle and stores the elements in two "
+              << "different lists using pass by reference\n"
               << std::endl;
 
     // ask the user whether they want to input the list or read it from a file
@@ -53,11 +52,11 @@ int main() {
     std::cout << "Do you want to input or read from a file (Input, File): ";
     std::cin >> fileInput;
 
+    // if the user wants to read from a file
     if (fileInput == "File" || fileInput == "file") {
-        // if the user wants to read from a file
         try {
             // open the file in read mode
-            std::ifstream f("list.txt");
+            std::ifstream f("test.txt");
             // read the first line of the file
             std::string listString;
             std::getline(f, listString);
@@ -76,35 +75,43 @@ int main() {
             // call the SplitList function to split the list in the middle
             SplitList(lst, half1, half2);
 
+            // calculate the middle index of the list
+            float middle = lst.size() / 2.0;
+            middle = ceil(middle);            // round up to the nearest integer
+
             // print the two halves of the list
-            std::cout << "First half: ";
-            for (std::string x : half1) {
-                std::cout << x << " ";
+            std::cout << "First half: [";
+            for (int i = 1; i < middle; i++) {
+                auto it = lst.begin();
+                std::advance(it, i);
+                std::cout << i << ", ";
             }
-            std::cout << std::endl
-                      << "Second half: ";
-            for (std::string x : half2) {
-                std::cout << x << " ";
+            std::cout << half1.back() << "]" << std::endl;
+
+            std::cout << "Second half: [";
+            for (int i = middle + 1; i < lst.size(); i++) {
+                auto it = lst.begin();
+                std::advance(it, i);
+                std::cout << i << ", ";
             }
-            std::cout << std::endl;
+            std::cout << half2.back() << "]" << std::endl;
+
 
             // close the file
             f.close();
         } catch (...) {
-            std::cout << "There was an error reading the file or parsing"
-            << " the list." << std::endl;
+            std::cout << "There was an error reading the file." << std::endl;
         }
     } else {  // if the user wants to input or they just input incorrectly
         // get list of numbers
         std::string listString, num, extra;
         std::list<std::string> lst;
-        std::cout << "Enter a list of elements (seperated by spaces): ";
+        std::cout << "Enter a list of elements (separated by spaces): ";
         std::cin >> listString;
-        // Read the rest of the line of input (including spaces)
+        // Read the entire line of input (including spaces)
         try {
             std::getline(std::cin, extra);
 
-            // concastinate listString and extra
             listString += extra;
 
             // Use a stringstream to parse the input string
@@ -130,17 +137,26 @@ int main() {
             // call the SplitList function to split the list in the middle
             SplitList(lst, half1, half2);
 
+
             // print the two halves of the list
-            std::cout << "First half: ";
+            std::cout << "First half: [";
+            std::string z = half1.back();
+            half1.pop_back();
+
             for (std::string x : half1) {
-                std::cout << x << " ";
+                std::cout << "'" << x << "', ";
             }
-            std::cout << std::endl
-                      << "Second half: ";
+            std::cout << "'" << z << "']" << std::endl;
+
+
+            std::cout << "Second half: [";
+            z = half2.back();
+            half2.pop_back();
+
             for (std::string x : half2) {
-                std::cout << x << " ";
+                std::cout <<"'" << x << "', ";
             }
-            std::cout << std::endl;
+            std::cout << "'" << z << "']" << std::endl;
         } catch (...) {
             std::cout << "Error parsing the list" << std::endl;
         }
